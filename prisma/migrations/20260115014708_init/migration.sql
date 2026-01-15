208296029ba47fd2e9270136a426e2142bbd89d9
KleberMera <klebermera2016@gmail.com>
2026-01-14 20:52:26 -0500

    feat: Incorporación de compatibilidad con PostgreSQL con la integración de Prisma
    - Actualización de package.json para incluir las dependencias de Prisma y PostgreSQL.
    - Creación de prisma.config.ts para la configuración de Prisma.
    - Migración inicial para las tablas Usuario y Roles con restricciones de clave externa.
    - Creación de un archivo de bloqueo de migración para el control de versiones.
    - Definición del esquema de Prisma con los modelos Usuario y Roles.

---
+++ prisma/migrations/20260115014708_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "rolId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Roles" (
+    "id" SERIAL NOT NULL,
+    "nombre" TEXT NOT NULL,
+    "descripcion" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Roles_nombre_key" ON "Roles"("nombre");
+
+-- AddForeignKey
+ALTER TABLE "Usuario" ADD CONSTRAINT "Usuario_rolId_fkey" FOREIGN KEY ("rolId") REFERENCES "Roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
